445fd3916142cc692034a498a1e3e7f7d8b26c8e
Asmamraza18 <asmasalsabila180@gmail.com>
2024-11-05 12:18:20 +0800

    Your descriptive commit message

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import UserLogin from '../components/UserLogin';
+
+const LoginPage = () => {
+  return (
+    <div className='text-white h-[100vh] flex justify-center items-center bg-cyan-700'>
+      <UserLogin />
+    </div>
+  );
+};
+
+export default LoginPage;
